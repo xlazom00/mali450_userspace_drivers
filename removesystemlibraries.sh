89d82db0f489369e73830b95aee9e26f04bd9201
Michal Lazo <xlazom00@gmail.com>
2019-03-28 01:36:37 -0700

    remove system libraries script

---
+++ removesystemlibraries.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm /usr/lib/aarch64-linux-gnu/libEGL.so*
+rm /usr/lib/aarch64-linux-gnu/libGLESv1_CM.so*
+rm /usr/lib/aarch64-linux-gnu/libGLESv2.so*
+rm /usr/lib/aarch64-linux-gnu/libgbm.so*
+rm /usr/lib/aarch64-linux-gnu/libwayland-egl.so*
+
+rm /usr/lib/aarch64-linux-gnu/libEGL_mesa.so*
+rm /usr/lib/aarch64-linux-gnu/libGL.so*
+rm /usr/lib/aarch64-linux-gnu/libGLX.so*
+rm /usr/lib/aarch64-linux-gnu/libGLX_indirect.so*
+rm /usr/lib/aarch64-linux-gnu/libGLX_mesa.so*
+rm /usr/lib/aarch64-linux-gnu/libGLdispatch.so*
